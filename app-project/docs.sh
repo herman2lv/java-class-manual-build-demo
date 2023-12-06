e83f2b3bd3aef63a06bd9123110f41da477b875e
herman2lv <herman2lv@gmail.com>
2023-12-06 23:45:02 +0300

    Add javadocs stage

---
+++ app-project/docs.sh
@@ -0,0 +1,1 @@
+javadoc -d docs -cp ../libs/hello-util.jar:src/ ./src/com/hrm/app/App.java
--- app-project/src/com/hrm/app/App.java
+++ app-project/src/com/hrm/app/App.java
@@ -2,7 +2,22 @@ package com.hrm.app;
 
 import com.hrm.util.HelloUtil;
 
+/**
+ * Some class description. With some additional information.
+ */
 public class App {
+	/**
+	 * Some method sescription. With some additional information
+	 *
+	 * <p> A paragraph inside javadoc.
+	 *
+	 * <ul>
+	 * 	<li>Some list item</li>
+	 * 	<li>One more list item</li>
+	 * </ul>
+	 *
+	 * @param args	the arguments of the application.
+	 */
     public static void main(String[] args) {
         HelloUtil.sayHello();
     }
